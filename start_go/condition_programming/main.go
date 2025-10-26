08147d0de906517f14d0def8a8c8a9683e297aa9
rakib-utsho <rakibutsho1920@gmail.com>
2025-10-26 12:36:20 +0600

    if else condition

---
+++ start_go/condition_programming/main.go
@@ -0,0 +1,18 @@
+package main
+
+//if...else and switch
+
+import "fmt"
+
+func main() {
+
+	age := 5
+
+	if age > 18 {
+		fmt.Println("Your are eligible to be married")
+	} else if age < 18 {
+		fmt.Println("You are not eligible to be married, but you can love someone")
+	} else {
+		fmt.Println("you are just a teenager, not eligible to be married")
+	}
+}
