644a7a21168052140ba7bb531e5d887632e1fb8e
rakib-utsho <rakibutsho1920@gmail.com>
2025-10-26 12:06:44 +0600

    varible declare

--- start_go/main.go
+++ start_go/main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-func  main()  {
-	fmt.Println("Hello world!")
+func main() {
+	// fmt.Println("Hello world!")
+
+	/*
+	 variable data type:
+	 int
+	 float 32
+	 bool
+	 string
+	*/
+
+	// variable declare
+	// var x int = 10
+	// x := 10
+	// x := 40.24
+	// x := "hello word"
+	x := true
+
+	fmt.Println(x)
 }
