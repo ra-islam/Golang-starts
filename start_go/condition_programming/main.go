fca635449aceb1445c5d0c042e78c81f783d4e9c
rakib-utsho <rakibutsho1920@gmail.com>
2025-10-28 19:29:04 +0600

    function  calling

--- start_go/condition_programming/main.go
+++ start_go/condition_programming/main.go
@@ -4,7 +4,18 @@ package main
 
 import "fmt"
 
+func add(num1 int, num2 int) {
+	sum := num1 + num2
+
+	fmt.Println(sum)
+}
+
 func main() {
+
+	a := 10
+	b := 20
+
+	add(a, b)
 
 	//conditional sign
 
@@ -13,13 +24,14 @@ func main() {
 	//or => ||
 	//not => !
 
-	age := 5
+	// age := 5
 
-	if age > 18 {
-		fmt.Println("Your are eligible to be married")
-	} else if age < 18 {
-		fmt.Println("You are not eligible to be married, but you can love someone")
-	} else {
-		fmt.Println("you are just a teenager, not eligible to be married")
-	}
+	// if age > 18 {
+	// 	fmt.Println("Your are eligible to be married")
+	// } else if age < 18 {
+	// 	fmt.Println("You are not eligible to be married, but you can love someone")
+	// } else {
+	// 	fmt.Println("you are just a teenager, not eligible to be married")
+	// }
+
 }
